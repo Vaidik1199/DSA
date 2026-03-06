98384cd7bca225034dda05dc3823c9aa23e4ee74
dev <vaidikhitendrachoudhary@gmail.com>
2026-03-06 23:01:32 +0530

    updating Greeting.java

--- JavaBasics/src/Greeting.java
+++ JavaBasics/src/Greeting.java
@@ -1,6 +1,6 @@
 public class Greeting {
     public static void main(String[] args) {
-        Greet():
+        Greet();
     }
     static void Greet() {
         System.out.println("Hello there, this is just a greeting form my side !!!");
