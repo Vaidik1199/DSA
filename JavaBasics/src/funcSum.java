341c914e59d86e6f22904d004763b43acfbb79e0
dev <vaidikhitendrachoudhary@gmail.com>
2026-03-07 00:13:15 +0530

    updating funcSum.java

--- JavaBasics/src/funcSum.java
+++ JavaBasics/src/funcSum.java
@@ -3,8 +3,26 @@ import java.util.Scanner;
 public class funcSum {
     public static void main(String[] args) {
         // In java The functions are called as methods
-        sum();
+//      sum();    to simply call!!!
+        int ans = sum2();
+        System.out.println(ans);
     }
+
+    // return the value if needed
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1 = in.nextInt();
+        System.out.println("Enter the Second number");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum;
+
+        // after returing khatam,,,, nothing will execute here after reuturn
+        // this is dead land
+    }
+
+
     static void sum() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the first number");
