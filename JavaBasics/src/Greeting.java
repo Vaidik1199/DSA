bb20433692da1cb13efaf6f2a5165d694536a119
dev <vaidikhitendrachoudhary@gmail.com>
2026-03-06 23:00:44 +0530

    adding Greeting.java

---
+++ JavaBasics/src/Greeting.java
@@ -0,0 +1,8 @@
+public class Greeting {
+    public static void main(String[] args) {
+        Greet():
+    }
+    static void Greet() {
+        System.out.println("Hello there, this is just a greeting form my side !!!");
+    }
+}
