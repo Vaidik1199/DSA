ab406144578491b3f62ff7dc836698f807f32073
dev <vaidikhitendrachoudhary@gmail.com>
2026-03-07 23:04:51 +0530

    creating VarArgs.java

---
+++ JavaBasics/src/VarArgs.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+
+        multiple(2,3,"four","five","yo-yo");
+
+        fun(2,3,4,5,6,34,5,32,255,64,1,2,3);
+
+    }
+
+    // mixed arguments like known and not know
+    static void multiple(int a, int b, String ...v){
+    }
+
+    // variable number of arguments
+    static void fun(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
+}
