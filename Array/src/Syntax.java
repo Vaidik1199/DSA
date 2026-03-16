10e9f41e7e1c29114be619a73412c93c0918336d
dev <vaidikhitendrachoudhary@gmail.com>
2026-03-16 23:51:44 +0530

    creating Syntax.java

---
+++ Array/src/Syntax.java
@@ -0,0 +1,20 @@
+public class Syntax {
+    public static void main(String[] args) {
+
+        //  Syntax
+
+        // datatype[] variable_name = new datatype[size];
+
+        // store 5 roll no
+        int[] rollno = new int[5];
+        // or
+        int[] rno = {12,34,22,13};
+
+        System.out.println(rollno[1]);  // o/p 0
+
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);   // o/p null
+
+    }
+}
