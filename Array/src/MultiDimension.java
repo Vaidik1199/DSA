7ce3f9d4758b9d01ababa0d19d79ffaa179ed1ee
dev <vaidikhitendrachoudhary@gmail.com>
2026-03-17 20:59:25 +0530

    creating MultiDimension.java

---
+++ Array/src/MultiDimension.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultiDimension {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        /*
+               1  2  3
+               4  5  6
+               7  8  9
+         */
+
+        // adding the rows is mandatory but columns are optional
+        int[][] arr = new int[3][3];
+
+        // another type
+        int[][] arr1 = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        // myth clear the array is not a matrix
+        int [][] arr2D = {
+                {1,2,3},  //0th index
+                {4,5},    //1st index
+                {6,7,8,9} //2nd index
+        };
+
+        System.out.println(Arrays.toString(arr2D[2]));  // [6,7,8,9]
+        System.out.println(arr2D[2][0]);   //6
+
+
+        // for input in the array
+        int[][] arr3 = new int[3][2];
+
+        // taking input
+        for (int row = 0; row < arr3.length; row++) {
+            // for each col in every row
+            for (int col = 0; col < arr3[row].length; col++) {
+                arr[row][col] =in.nextInt();
+            }
+        }
+    }
+}
