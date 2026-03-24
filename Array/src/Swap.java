afe3c91ed166326945f8e9eef51631518150e187
dev <vaidikhitendrachoudhary@gmail.com>
2026-03-24 20:23:29 +0530

    solving swapping question

---
+++ Array/src/Swap.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr={1,3,23,9,18};
+        swap(arr,2,4);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void swap(int[] arr, int i1, int i2) {
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
+    }
+}
