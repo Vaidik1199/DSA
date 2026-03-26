873749e3d389ac74ab6f8b8e707892803d963279
dev <vaidikhitendrachoudhary@gmail.com>
2026-03-26 18:13:36 +0530

    solved simple linear search question to understand

--- LinearSearch/src/first.java
+++ LinearSearch/src/first.java
@@ -1,11 +1,32 @@
 public class first {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int[] nums = {23,45,1,2,8,19,-3,16,-11,28};
+        int target = 19;
+        int ans = linearSearch(nums,target);
+        System.out.println(ans);
     }
 
     // search in the array
     // will return the index if item found
     // will return -1 if not found
-    static int linearsearch(int[] arr){}
+    static int linearSearch(int[] arr, int target){
 
+        // if arr length is zero
+        if(arr.length == 0) {
+            return -1;
+        }
+
+        // run a for loop
+        for (int index = 0; index < arr.length; index++) {
+            // check for element at every index if it is = target
+            int element = arr[index];
+            if (element == target) {
+                return index;
+            }
+        }
+        // this will execute of none of the return statement above have executed
+        // hence the target is not found
+        return -1;
+    }
 }
