8013f814e6ba4179f36144e1269e3aa2c4a20bea
dev <vaidikhitendrachoudhary@gmail.com>
2026-03-26 17:58:42 +0530

    created first program name first.java

---
+++ LinearSearch/src/first.java
@@ -0,0 +1,11 @@
+public class first {
+    public static void main(String[] args) {
+
+    }
+
+    // search in the array
+    // will return the index if item found
+    // will return -1 if not found
+    static int linearsearch(int[] arr){}
+
+}
