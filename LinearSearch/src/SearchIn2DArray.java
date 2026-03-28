5bcc438e1ead0e6cde228af6239cbcb09e9b202d
dev <vaidikhitendrachoudhary@gmail.com>
2026-03-28 18:57:34 +0530

    solved by me yo yo yo

---
+++ LinearSearch/src/SearchIn2DArray.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class SearchIn2DArray {
+    public static void main(String[] args) {
+        int[][] arr= {
+                {23,4,1},
+                {18,12,3,9},
+                {78,99,34,56},
+                {18,12}
+        };
+        int target = 1;
+        System.out.println(search(arr,target));
+    }
+
+    public static String search(int[][] arr, int target) {
+        if (arr.length==0) {
+            return "not applicable";
+        }
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col]==target){
+                    String ans =row+","+col;
+                    return ans;
+                }
+            }
+        }
+        return "Error";
+    }
+}
