c86c84ba6fbd05fce5cde56f9a07aaf643a379df
dev <vaidikhitendrachoudhary@gmail.com>
2026-04-04 18:54:56 +0530

    solving basic question to get hands on with concepts and to minimize delay

---
+++ FoundationQuestion/src/loopsArrays/PrintAllElementsOfArray.java
@@ -0,0 +1,10 @@
+package loopsArrays;
+public class PrintAllElementsOfArray {
+    public static void main(String[] args) {
+        int[] arr = {23,45,76,45};
+
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
