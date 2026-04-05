0ae2da72549744dd9c7c32a20595c5a2095afdbd
dev <vaidikhitendrachoudhary@gmail.com>
2026-04-05 18:48:16 +0530

    solved the question tell array is sorted or not

---
+++ FoundationQuestion/src/ControlLogicBuilding/CheckIfArrayIsSorted.java
@@ -0,0 +1,17 @@
+package ControlLogicBuilding;
+
+public class CheckIfArrayIsSorted {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,7,6};
+        System.out.println(IsSorted(arr));
+    }
+
+    private static boolean IsSorted(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
