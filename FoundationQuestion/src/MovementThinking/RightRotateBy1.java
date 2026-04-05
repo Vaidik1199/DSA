77f99ca10e9335735c9329e3f5a41741184126dd
dev <vaidikhitendrachoudhary@gmail.com>
2026-04-05 17:36:27 +0530

    solved this question RightRotateBy1.java,

---
+++ FoundationQuestion/src/MovementThinking/RightRotateBy1.java
@@ -0,0 +1,13 @@
+package MovementThinking;
+import java.util.Arrays;
+public class RightRotateBy1 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int old = arr[arr.length-1];
+        for (int i = arr.length-1; i > 0; i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[0]=old;
+        System.out.println(Arrays.toString(arr));
+    }
+}
