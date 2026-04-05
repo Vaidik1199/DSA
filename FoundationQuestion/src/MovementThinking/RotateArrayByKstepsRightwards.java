757c3c8dbc1e4cd50bb23264fa38bba8f8b13b99
dev <vaidikhitendrachoudhary@gmail.com>
2026-04-05 18:23:08 +0530

    solved this question RotateArrayByKstepsRightwards.java,

---
+++ FoundationQuestion/src/MovementThinking/RotateArrayByKstepsRightwards.java
@@ -0,0 +1,25 @@
+package MovementThinking;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class RotateArrayByKstepsRightwards {
+    public static void main(String[] args) {
+        int[] arr= {1,2,3,4,5,6,7,8};
+        int k = 3;
+
+        for (int i = 0; i < k; i++) {
+            Rotate(arr);
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void Rotate(int[] arr) {
+        int old = arr[arr.length-1];
+        for (int i = arr.length-1; i > 0; i--) {
+            arr[i]=arr[i-1];
+        }
+
+        arr[0] = old;
+    }
+}
